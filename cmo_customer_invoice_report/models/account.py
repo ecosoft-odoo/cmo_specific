a1a283bfc4f912521ca3695cc5adf0dea1603fff
Kitti U <kittiu@gmail.com>
2017-12-11 15:41:20 +0700

    [ENH] rename forms #736

--- cmo_customer_invoice_report/models/account.py
+++ cmo_customer_invoice_report/models/account.py
@@ -2,18 +2,19 @@
 from openerp import models, api
 
 
+def filter_print_report(res, reports):
+    action = []
+    if res.get('toolbar', False) and \
+            res.get('toolbar').get('print', False):
+        for act in res.get('toolbar').get('print'):
+            if act.get('report_name') in reports:
+                action.append(act)
+        res['toolbar']['print'] = action
+    return res
+
+
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
-
-    def filter_print_report(self, res, reports):
-        action = []
-        if res.get('toolbar', False) and \
-                res.get('toolbar').get('print', False):
-            for act in res.get('toolbar').get('print'):
-                if act.get('name') in reports:
-                    action.append(act)
-            res['toolbar']['print'] = action
-        return res
 
     @api.model
     def fields_view_get(self, view_id=None, view_type='form',
@@ -25,45 +26,35 @@ class AccountInvoice(models.Model):
                 self._context.get('type', False) == 'out_invoice' and\
                 self._context.get('journal_type', False) == 'sale':
             reports = [
-                u'Customer/Invoice',
-                u'Customer/Invoice(Project)',
+                u'cmo.customer.invoice',
+                u'cmo.customer.invoice2',
             ]
-            self.filter_print_report(res, reports)
+            filter_print_report(res, reports)
         # Customer Refund
         elif self._context.get('default_type', False) == 'out_refund' and\
                 self._context.get('type', False) == 'out_refund' and\
                 self._context.get('journal_type', False) == 'sale_refund':
             reports = [
-                u'Customer/Refund',
+                u'cmo.customer.refund',
             ]
-            self.filter_print_report(res, reports)
+            filter_print_report(res, reports)
         # Suplier Invoice
         elif self._context.get('default_type', False) == 'in_invoice' and\
                 self._context.get('type', False) == 'in_invoice' and\
                 self._context.get('journal_type', False) == 'purchase':
             reports = []
-            self.filter_print_report(res, reports)
+            filter_print_report(res, reports)
         # Suplier Refund
         elif self._context.get('default_type', False) == 'in_refund' and\
                 self._context.get('type', False) == 'in_refund' and\
                 self._context.get('journal_type', False) == 'purchase_refund':
             reports = []
-            self.filter_print_report(res, reports)
+            filter_print_report(res, reports)
         return res
 
 
 class AccountVoucher(models.Model):
     _inherit = 'account.voucher'
-
-    def filter_print_report(self, res, reports):
-        action = []
-        if res.get('toolbar', False) and \
-                res.get('toolbar').get('print', False):
-            for act in res.get('toolbar').get('print'):
-                if act.get('name') in reports:
-                    action.append(act)
-            res['toolbar']['print'] = action
-        return res
 
     @api.model
     def fields_view_get(self, view_id=None, view_type='form',
@@ -73,24 +64,24 @@ class AccountVoucher(models.Model):
         # Customer Payment
         if self._context.get('type', False) == 'receipt':
             reports = [
-                u'Customer/Payment',
+                u'cmo.customer.payment',
             ]
-            self.filter_print_report(res, reports)
+            filter_print_report(res, reports)
         # Customer Receipt
         elif self._context.get('type', False) == 'sale' and\
                 self._context.get('default_type', False) == 'sale':
             reports = []
-            self.filter_print_report(res, reports)
+            filter_print_report(res, reports)
         # Suplier Payment
         elif self._context.get('type', False) == 'payment':
             reports = [
-                u'Supplier/Payment(Cheque)',
-                u'Payment/Voucher',
+                u'cmo.supplier.payment.cheque',
+                u'cmo.supplier.payment.voucher',
             ]
-            self.filter_print_report(res, reports)
+            filter_print_report(res, reports)
         # Customer Receipt
         elif self._context.get('type', False) == 'purchase' and\
                 self._context.get('default_type', False) == 'purchase':
             reports = []
-            self.filter_print_report(res, reports)
+            filter_print_report(res, reports)
         return res
--- cmo_hr_expense_report/models/hr.py
+++ cmo_hr_expense_report/models/hr.py
@@ -2,19 +2,19 @@
 from openerp import models, api
 
 
+def filter_print_report(res, reports):
+    action = []
+    if res.get('toolbar', False) and \
+            res.get('toolbar').get('print', False):
+        for act in res.get('toolbar').get('print'):
+            if act.get('report_name') in reports:
+                action.append(act)
+        res['toolbar']['print'] = action
+    return res
+
+
 class HrExpenseExpense(models.Model):
     _inherit = 'hr.expense.expense'
-
-    @api.model
-    def filter_print_report(self, res, reports):
-        action = []
-        if res.get('toolbar', False) and \
-                res.get('toolbar').get('print', False):
-            for act in res.get('toolbar').get('print'):
-                if act.get('name') in reports:
-                    action.append(act)
-            res['toolbar']['print'] = action
-        return res
 
     @api.model
     def fields_view_get(self, view_id=None, view_type='form',
@@ -26,20 +26,20 @@ class HrExpenseExpense(models.Model):
                 not self._context.get('is_employee_advance', False) and \
                 not self._context.get('default_is_advance_clearing', False):
             reports = [
-                u'HR/Expense',
+                u'cmo.hr.expense',
             ]
-            self.filter_print_report(res, reports)
+            filter_print_report(res, reports)
         # HR Advance
         elif self._context.get('default_is_employee_advance', False) and\
                 self._context.get('is_employee_advance', False):
             reports = [
-                u'HR/Advance',
+                u'cmo.hr.advance',
             ]
-            self.filter_print_report(res, reports)
+            filter_print_report(res, reports)
         # HR Advance Clearing
         elif self._context.get('default_is_employee_advance', False) is False \
                 and self._context.get('default_is_advance_clearing', False) \
                 and self._context.get('is_advance_clearing', False):
             reports = []
-            self.filter_print_report(res, reports)
+            filter_print_report(res, reports)
         return res
