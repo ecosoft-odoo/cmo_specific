5f728feef9554b06d1d33f63c7bc3b67108ef14c
newtratip <tharathip.chaweewongphan@gmail.com>
2018-03-13 11:20:03 +0700

    [IMP] Adjust domain partner_id

--- cmo_stock/models/res_partner.py
+++ cmo_stock/models/res_partner.py
@@ -6,7 +6,7 @@ class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     @api.model
-    def name_search(self, name, args=None, operator='ilike', limit=100):
+    def _get_domain(self, domain):
         context = self._context.copy()
         if context.get('group_partner_operating_unit', False):
             users = self.env.user
@@ -14,7 +14,26 @@ class ResPartner(models.Model):
             users = self.env['res.users'].search(
                 [('default_operating_unit_id', '=', operating_unit_id)])
             partner_ids = users.mapped('partner_id.id')
-            args = [('id', 'in', partner_ids)] + args
+            domain = [('id', 'in', partner_ids)] + domain
+        return domain
+
+    @api.model
+    def name_search(self, name, args=None, operator='ilike', limit=100):
         return super(ResPartner, self).name_search(
-            name, args=args, operator=operator, limit=limit
-        )
+            name, args=self._get_domain(args), operator=operator, limit=limit)
+
+    @api.model
+    def search_read(self, domain=None, fields=None, offset=0,
+                    limit=None, order=None):
+        res = super(ResPartner, self).search_read(
+            domain=self._get_domain(domain), fields=fields, offset=offset,
+            limit=limit, order=order)
+        return res
+
+    @api.model
+    def read_group(self, domain, fields, groupby, offset=0, limit=None,
+                   orderby=False, lazy=True):
+        res = super(ResPartner, self).read_group(
+            self._get_domain(domain), fields, groupby, offset=offset,
+            limit=limit, orderby=orderby, lazy=lazy)
+        return res
