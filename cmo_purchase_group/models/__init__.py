917b5a29ca5eb8df8bf3a6393b176bd229153c57
newtratip <tharathip.chaweewongphan@gmail.com>
2018-03-27 14:25:10 +0700

    [IMP] Adjust readonly group

--- cmo_purchase_group/models/__init__.py
+++ cmo_purchase_group/models/__init__.py
@@ -1,7 +1,3 @@
 # -*- coding: utf-8 -*-
 from . import common
 from . import purchase
-from . import res_partner
-from . import stock
-from . import account_invoice
-from . import product
--- cmo_sale_group/models/sale.py
+++ cmo_sale_group/models/sale.py
@@ -17,3 +17,19 @@ class SaleOrder(models.Model, Common):
         if user.has_group(sale_readonly_group):
             res = self.set_right_readonly_group(res)
         return res
+
+
+class SaleOrderLine(models.Model, Common):
+    _inherit = 'sale.order.line'
+
+    @api.model
+    def fields_view_get(self, view_id=None, view_type='form',
+                        toolbar=False, submenu=False):
+        res = super(SaleOrderLine, self).fields_view_get(
+            view_id, view_type, toolbar=toolbar, submenu=submenu)
+        user = self.env.user
+        sale_readonly_group = \
+            'base.group_sale_salesman_all_leads_all_ou_readonly'
+        if user.has_group(sale_readonly_group):
+            res = self.set_right_readonly_group(res)
+        return res
