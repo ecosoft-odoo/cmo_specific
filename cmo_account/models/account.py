dd015cf3597680d5a5d58b0746b35eb9b7175a56
som <jutamatk@ecosoft.co.th>
2018-07-03 12:07:22 +0700

    add field payee, hide fields ref. and partner, search for payee

--- cmo_account/models/account.py
+++ cmo_account/models/account.py
@@ -59,12 +59,19 @@ class AccountMoveLine(models.Model):
         related='move_id.ref_voucher_id.invoices_ref',
     )
 
+    voucher_payee = fields.Char(
+        string='Payee',
+        compute='_compute_voucher_ref',
+        search='_search_voucher_payee',
+    )
+
     @api.multi
     def _compute_voucher_ref(self):
         for rec in self:
             voucher = rec.move_id.ref_voucher_id
             rec.voucher_number_cheque = voucher.number_cheque
             rec.voucher_date_value = voucher.date_value
+            rec.voucher_payee = voucher.payee
 
     @api.model
     def _search_voucher_number_cheque(self, operator, value):
@@ -95,4 +102,35 @@ class AccountMoveLine(models.Model):
         if operator == '!=':
             lines = lines.filtered(
                 lambda l: value != l.move_id.ref_voucher_id.number_cheque)
+        return [('id', 'in', lines.ids)]
+
+    @api.model
+    def _search_voucher_payee(self, operator, value):
+        context = self._context.copy()
+        currency_id = context.get('currency_none_same_company_id', False)
+        journal_id = context.get('journal_id', False)
+        account_id = context.get('journal_default_account_id', False)
+        domain = [
+            ('reconcile_id', '=', False),
+            ('credit', '>', 0),
+            ('currency_id', '=', currency_id),
+            ('journal_id', '=', journal_id),
+            ('account_id', '=', account_id)]
+        lines = self.search(domain)
+        if operator == 'ilike':
+            lines = lines.filtered(
+                lambda l:
+                l.move_id.ref_voucher_id.payee is not False and
+                value in l.move_id.ref_voucher_id.payee)
+        if operator == 'not ilike':
+            lines = lines.filtered(
+                lambda l: l.move_id.ref_voucher_id.payee is False or
+                (l.move_id.ref_voucher_id.payee is not False and
+                 value not in l.move_id.ref_voucher_id.payee))
+        if operator == '=':
+            lines = lines.filtered(
+                lambda l: value == l.move_id.ref_voucher_id.payee)
+        if operator == '!=':
+            lines = lines.filtered(
+                lambda l: value != l.move_id.ref_voucher_id.payee)
         return [('id', 'in', lines.ids)]
