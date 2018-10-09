7dab6aee465719c92eeba5f85dda83091eb06075
newtratip <tharathip.chaweewongphan@gmail.com>
2018-10-09 14:33:44 +0700

    [IMP] Add cmo__pre_go_live

---
+++ cmo__pre_go_live/models/account_asset_line.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields
+
+
+class AccountAssetLine(models.Model):
+    _inherit = 'account.asset.line'
+
+    line_days = fields.Integer(
+        readonly=False,
+    )
