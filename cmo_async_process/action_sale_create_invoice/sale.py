f05e81ad96c256b4ea467f1302199bb3442d2e7c
Kitti U <kittiu@gmail.com>
2018-12-14 16:58:39 +0700

    -

--- cmo_async_process/action_sale_create_invoice/sale.py
+++ cmo_async_process/action_sale_create_invoice/sale.py
@@ -20,7 +20,7 @@ def related_sale_order(session, thejob):
     return action
 
 
-@job(default_channel='root.sale_create_invoice')
+@job
 @related_action(action=related_sale_order)
 def action_sale_manual_invoice(session, model_name, res_id):
     try:
