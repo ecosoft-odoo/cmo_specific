28aa46df7bbbededf137c9874b6d7e06bb1a8066
newtratip <tharathip.chaweewongphan@gmail.com>
2019-03-06 09:48:32 +0700

    [IMP] Add filter fiscalyear in domain init_move and focus_move

--- account_trial_balance_report/models/trial_balance_report.py
+++ account_trial_balance_report/models/trial_balance_report.py
@@ -84,7 +84,8 @@ class AccountTrailBalanceReport(models.Model):
         domain = [('account_id', '=', account.id),
                   '|', ('journal_id.centralisation', '=', True),
                   '&', ('journal_id.centralisation', '=', False),
-                  ('date', '<', report.date_start)]
+                  ('date', '<', report.date_start),
+                  ('period_id.fiscalyear_id', '=', report.fiscalyear_id.id)]
         if charge_type:
             domain += [('charge_type', '=', charge_type)]
         if target_move == 'posted':
@@ -98,7 +99,8 @@ class AccountTrailBalanceReport(models.Model):
         domain = [('account_id', '=', account.id),
                   ('journal_id.centralisation', '=', False),
                   ('date', '>=', report.date_start),
-                  ('date', '<=', report.date_stop)]
+                  ('date', '<=', report.date_stop),
+                  ('period_id.fiscalyear_id', '=', report.fiscalyear_id.id)]
         if charge_type:
             domain += [('charge_type', '=', charge_type)]
         if target_move == 'posted':
