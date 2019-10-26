56a85bcfcfa274fd22fad14b1890b6127126b54b
newtratip <tharathip.chaweewongphan@gmail.com>
2019-10-26 16:39:14 +0700

    [8.0][ADD] Customer Receipt Voucher Report

---
+++ cmo_customer_receipt_voucher_report/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'CMO :: Customer Receipt Voucher Report',
+    'version': '8.0.1.0.0',
+    'category': 'Accounting & Finance',
+    'description': """
+""",
+    'author': 'Tharathip C.',
+    'website': 'http://ecosoft.co.th',
+    'depends': [
+        'account_voucher',
+    ],
+    'data': [
+        'data/report_paperformat_data.xml',
+        'data/report_data.xml',
+        'report/report_templates.xml',
+        'report/report_customer_receipt_voucher.xml',
+        'wizards/customer_receipt_voucher_wizards.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
