39a633334ea43a038dcc50e35f71eae2d77554fa
kongrattapong <rattapong.chok@gmail.com>
2020-01-07 14:42:34 +0700

    [ENH] related user

---
+++ cmo_related_partner/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'CMO :: Related Partner',
+    'summary': '',
+    'author': 'Rattapong C.',
+    'website': 'http://ecosoft.co.th',
+    'version': '8.0.1.0.0',
+    'depends': ['base'],
+    'data': [
+        'views/res_user_views.xml',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+}
