1b116b2d9a16994fc7ed73dcbc6cf793c22d2d9f
newtratip <tharathip.chaweewongphan@gmail.com>
2020-04-07 16:03:22 +0700

    [8.0][UPD] cmo_customer_invoice_report

--- cmo_customer_invoice_report/models/account.py
+++ cmo_customer_invoice_report/models/account.py
@@ -36,8 +36,8 @@ class AccountInvoice(models.Model):
                 u'cmo.customer.invoice2.en',
                 u'cmo.customer.invoice2.th',
                 u'cmo.sale.daybook',
-                # u'cmo.receipt.tax.invoice.peo.en',
-                # u'cmo.receipt.tax.invoice.peo.th',
+                u'cmo.receipt.tax.invoice.peo.en',
+                u'cmo.receipt.tax.invoice.peo.th',
                 u'cmo.receipt.en',
                 u'cmo.receipt.th',
             ]
