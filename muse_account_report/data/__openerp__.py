35a9ebd4fada2613eb5ee9a58058dcdc96716d55
watcharaporn-git <watcha076@gmail.com>
2020-07-02 11:00:36 +0700

    [ADD] inti and openerp

---
+++ muse_account_report/data/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'MUSE :: Account Report',
+    'author': 'Ecosoft',
+    'website': 'http://ecosoft.co.th',
+    'category': 'Account',
+    'version': '8.0.1.0',
+    'depends': [
+        'l10n_th_account',
+    ],
+    'data': [
+        'data/report_data.xml'
+    ],
+    'installable': True,
+}
