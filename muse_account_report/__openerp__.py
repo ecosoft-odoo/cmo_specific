179afda509dd87424b0389f09b7e0c1b8a13dd63
newtratip <tharathip.chaweewongphan@gmail.com>
2020-07-03 12:09:39 +0700

    [8.0][UPD] Edit Witholding Tax Certificate

--- muse_account_report/__openerp__.py
+++ muse_account_report/__openerp__.py
@@ -4,9 +4,10 @@
     'author': 'Ecosoft',
     'website': 'http://ecosoft.co.th',
     'category': 'Account',
-    'version': '8.0.1.0',
+    'version': '8.0.1.0.0',
     'depends': [
         'l10n_th_account',
+        'cmo_customer_invoice_report',
     ],
     'data': [
         'data/report_data.xml'
