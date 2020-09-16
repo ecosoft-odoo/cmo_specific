4c7a794211342c4649d544e62ac2a92f5ecdb88e
Saran440 <saranl@ecosoft.co.th>
2020-09-16 09:23:46 +0700

    [FIX] customer_receipt_voucher_report

--- cmo_customer_receipt_voucher_report/wizards/customer_receipt_voucher.py
+++ cmo_customer_receipt_voucher_report/wizards/customer_receipt_voucher.py
@@ -10,9 +10,6 @@ class CustomerReceiptVoucher(models.TransientModel):
         'account.voucher',
         string='Customer Payments',
     )
-    date_due = fields.Date(
-        string='Due Date',
-    )
     partner_id = fields.Many2one(
         'res.partner',
         string='Partner',
@@ -21,22 +18,17 @@ class CustomerReceiptVoucher(models.TransientModel):
     is_cheque = fields.Boolean(
         string='Cheque ?',
     )
-    number_cheque = fields.Char(
-        string='Cheque No.',
-    )
 
     @api.multi
     def action_get_report(self):
         self.ensure_one()
         # Get Result Report
         Result = self.env['report.customer.receipt.voucher']
-        dom = [('voucher_id.number_cheque', '=', self.number_cheque)]
+        dom = []
         if self.partner_id:
             dom += [('voucher_id.partner_id', '=', self.partner_id.id)]
         if self.voucher_ids:
             dom += [('voucher_id', 'in', self.voucher_ids.ids)]
-        if self.date_due:
-            dom += [('voucher_id.date', '=', self.date_due)]
         result = Result.search(dom)
         if not result:
             raise UserError(_('Error!'), _('No Data.'))
