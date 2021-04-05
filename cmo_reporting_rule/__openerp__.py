55646b8aa88e88906401e2d7c8b144f6acf8f5cc
flook-chaiwat <kullapatc@ecosoft.co.th>
2021-04-05 13:18:27 +0700

    [ADD] cmo_reporting_rule

---
+++ cmo_reporting_rule/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'CMO :: Reporting Group',
+    'summary': 'Create Reporting Group',
+    'author': 'Kullapat C.',
+    'website': 'http://ecosoft.co.th',
+    'category': '',
+    'version': '8.0.1.0.0',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'security/reporting_security.xml',
+    ],
+    'installable': True,
+}
